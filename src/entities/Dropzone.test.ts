84ac8274000c62f45f558065cad2f60cdcbdd040
Roman Kyslyy <roman.kyslyy@invitae.com>
2021-07-05 22:47:37 +0300

    done Dropzone tests

--- src/entities/Dropzone.test.ts
+++ src/entities/Dropzone.test.ts
@@ -2,27 +2,6 @@ import { Dropzone } from "./Dropzone";
 import Item from "./Item";
 
 describe("Dropzone", () => {
-  //   let dropzoneElement: HTMLElement;
-  //   let itemElement: HTMLElement;
-
-  //   let dropzone: Dropzone;
-  //   let item: Item;
-  //   let mouseEvent: MouseEvent;
-
-  //   beforeEach(() => {
-  //     dropzoneElement = document.createElement("div");
-  //     dropzoneElement.style.width = "500px";
-  //     dropzoneElement.style.height = "400px";
-
-  //     itemElement = document.createElement("div");
-  //     itemElement.style.width = "100px";
-  //     itemElement.style.height = "50px";
-
-  //     dropzone = new Dropzone(dropzoneElement);
-  //     item = new Item(dropzone, itemElement);
-  //     mouseEvent = new MouseEvent("mousemove");
-  //   });
-
   it("should store an array of Item objects built from children", () => {
     const dropzoneElement = document.createElement("div");
     const children = [
