49f5a1f37b787f2dd28d9602ce198e3b679a42fe
Roman Kyslyy <roman.kyslyy@invitae.com>
2021-07-06 09:57:25 +0300

    DndController tests

--- src/entities/DndController.test.ts
+++ src/entities/DndController.test.ts
@@ -1,5 +1,6 @@
+import "../setup";
+
 import DndController from "./DndController";
-// import Item from "./Item";
 
 describe("DndController", () => {
   const animationSpeed = 100;
@@ -59,7 +60,7 @@ describe("DndController", () => {
   });
 
   describe("prepareDocument()", () => {
-    it("should add listeners to documents", () => {
+    it("should add listeners to document", () => {
       const spy = jest.spyOn(document, "addEventListener");
 
       dndController.prepareDocument();
@@ -72,7 +73,7 @@ describe("DndController", () => {
   });
 
   describe("cleanUp()", () => {
-    it("should add listeners to documents", () => {
+    it("should remove listeners from documents", () => {
       const spy = jest.spyOn(document, "removeEventListener");
 
       dndController.cleanUp();
@@ -84,15 +85,146 @@ describe("DndController", () => {
     });
   });
 
-  // describe("clearDraggedItem()", () => {
-  //   it("should exit early if draggedItem is null", () => {
-  //     const wrapperElement = document.createElement("div");
-  //     const dropzoneElement = document.createElement("div");
-  //     const itemElement = document.createElement("div");
+  describe("clearDraggedItem()", () => {
+    it("should exit early if draggedItem is null", () => {
+      const wrapperElement = document.createElement("div");
+      const dropzoneElement = document.createElement("div");
+      const itemElement = document.createElement("div");
 
-  //     dropzoneElement.appendChild(itemElement);
+      dropzoneElement.appendChild(itemElement);
+      wrapperElement.appendChild(dropzoneElement);
 
-  //     dndController.setup();
-  //   });
-  // });
+      dndController.setup(wrapperElement);
+
+      dndController.draggedItem = null;
+
+      jest.spyOn(dndController, "onItemDrop");
+      jest.spyOn(dndController, "repositionItems");
+
+      dndController.clearDraggedItem();
+
+      expect(dndController.repositionItems).not.toHaveBeenCalled();
+      expect(dndController.onItemDrop).not.toHaveBeenCalled();
+    });
+
+    it("should reset draggedItem, reposition items, notify parent about item drop and reset from/to locations", async () => {
+      const wrapperElement = document.createElement("div");
+      const dropzoneElement = document.createElement("div");
+      const itemElement0 = document.createElement("div");
+      const itemElement1 = document.createElement("div");
+
+      dropzoneElement.id = "bw-dz-a";
+      dropzoneElement.appendChild(itemElement0);
+      dropzoneElement.appendChild(itemElement1);
+      wrapperElement.appendChild(dropzoneElement);
+
+      dndController.setup(wrapperElement);
+
+      const dropzone = dndController.dropzones[0];
+      const draggedItem = dropzone.items[0];
+      jest.spyOn(draggedItem, "applyDefaultStyle");
+
+      dndController.draggedItem = draggedItem;
+      dndController.initialItemGrabLocation = { dropzone, index: 0 };
+      dndController.grabbedItemCurrentLocation = { dropzone, index: 1 };
+
+      jest.spyOn(dndController, "onItemDrop");
+      jest.spyOn(dndController, "repositionItems");
+
+      await dndController.clearDraggedItem();
+
+      expect(draggedItem.applyDefaultStyle).toHaveBeenCalled();
+      expect(dndController.draggedItem).toBeNull();
+      expect(dndController.repositionItems).toHaveBeenCalled();
+      expect(dndController.onItemDrop).toHaveBeenCalledWith(dropzone.id, 0, dropzone.id, 1);
+      expect(dndController.initialItemGrabLocation).toBeNull();
+      expect(dndController.grabbedItemCurrentLocation).toBeNull();
+    });
+  });
+
+  describe("rebuildDropzones()", () => {
+    it("should re-collect dropzones and reposition items without animation", () => {
+      const wrapperElement = document.createElement("div");
+
+      jest.spyOn(dndController, "prepareDropzonesAndItems");
+      jest.spyOn(dndController, "repositionItems");
+
+      dndController.setup(wrapperElement);
+      dndController.rebuildDropzones();
+
+      expect(dndController.prepareDropzonesAndItems).toHaveBeenCalled();
+      expect(dndController.repositionItems).toHaveBeenCalledWith(false);
+    });
+  });
+
+  describe("placeDraggedItemInNewDropzone()", () => {
+    it("should remove item from current dropzone, insert it to new dropzone, update current location and reposition items", () => {
+      const wrapperElement = document.createElement("div");
+      const dropzoneElement0 = document.createElement("div");
+      const itemElement0 = document.createElement("div");
+      const dropzoneElement1 = document.createElement("div");
+      const itemElement1 = document.createElement("div");
+
+      dropzoneElement0.id = "bw-dz-a";
+      dropzoneElement1.id = "bw-dz-b";
+      dropzoneElement0.appendChild(itemElement0);
+      dropzoneElement1.appendChild(itemElement1);
+      wrapperElement.appendChild(dropzoneElement0);
+      wrapperElement.appendChild(dropzoneElement1);
+
+      dndController.setup(wrapperElement);
+
+      const dropzone0 = dndController.dropzones[0];
+      const dropzone1 = dndController.dropzones[1];
+
+      const draggedItem = dropzone0.items[0];
+      const placingIndex = 1;
+
+      dndController.draggedItem = draggedItem;
+      dndController.grabbedItemCurrentLocation = { dropzone: dropzone0, index: 0 };
+
+      jest.spyOn(dndController, "repositionItems");
+
+      dndController.placeDraggedItemInNewDropzone(dropzone1, draggedItem, placingIndex);
+
+      expect(dropzone0.items).toHaveLength(0);
+      expect(dropzone1.items).toContain(draggedItem);
+      expect(dndController.grabbedItemCurrentLocation).toEqual({
+        dropzone: dropzone1,
+        index: placingIndex,
+      });
+      expect(dndController.repositionItems).toHaveBeenCalled();
+    });
+  });
+
+  describe("handleIndexSwitchInsideDropzone()", () => {
+    it("should switch item positions in dropzone, update current dragged item location and reposition items", () => {
+      const wrapperElement = document.createElement("div");
+      const dropzoneElement = document.createElement("div");
+      const itemElement0 = document.createElement("div");
+      const itemElement1 = document.createElement("div");
+
+      dropzoneElement.id = "bw-dz-a";
+      dropzoneElement.appendChild(itemElement0);
+      dropzoneElement.appendChild(itemElement1);
+      wrapperElement.appendChild(dropzoneElement);
+
+      dndController.setup(wrapperElement);
+
+      const dropzone = dndController.dropzones[0];
+      const dropzoneItem0 = dropzone.items[0];
+      const dropzoneItem1 = dropzone.items[1];
+      const from = 0;
+      const to = 1;
+
+      jest.spyOn(dndController, "repositionItems");
+
+      dndController.handleIndexSwitchInsideDropzone(dropzone, from, to);
+
+      expect(dropzone.items[0]).toBe(dropzoneItem1);
+      expect(dropzone.items[1]).toBe(dropzoneItem0);
+      expect(dndController.grabbedItemCurrentLocation).toEqual({ dropzone, index: to });
+      expect(dndController.repositionItems).toHaveBeenCalled();
+    });
+  });
 });
